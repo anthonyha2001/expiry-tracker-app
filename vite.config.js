b04b0fca7e5a6a96f72cd3552984dafaf4021d51
Anthony Hasrouny <AnthonyHasrouny8@gmail.com>
2025-10-09 15:01:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set the base path to the root directory for services like Vercel
-  base: '/', 
+  // Set the base path for deployment on GitHub Pages.
+  // This is crucial for the live site to find its assets.
+  base: '/expiry-tracker-app/', 
 })
