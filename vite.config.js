02954f72c91412a0711741ae844b95d486311748
Anthony Hasrouny <AnthonyHasrouny8@gmail.com>
2025-10-09 14:52:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // Set the base path for deployment on GitHub Pages.
-  // This is crucial for the live site to find its assets.
-  base: '/expiry-tracker-app/',
-
-  // Added server configuration for ngrok
-  server: {
-    // This allows any URL ending in .ngrok-free.app to connect,
-    // which is useful for testing your site on other devices.
-    allowedHosts: ['.ngrok-free.dev'],
-  },
+  // Set the base path to the root directory for services like Vercel
+  base: '/', 
 })
